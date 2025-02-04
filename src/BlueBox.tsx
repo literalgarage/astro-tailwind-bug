b12296684e4c6b1551e22a4b1d19b468fc7f70ce
Dave <davepeck@gmail.com>
2025-02-04 14:42:19 -0800

    Minimal reproducible example

---
+++ src/BlueBox.tsx
@@ -0,0 +1,7 @@
+export default function BlueBox() {
+  return (
+    <div className="bg-blue-200 h-64 w-64">
+      This text should be on blue box. If it isn't, something is buggy.
+    </div>
+  );
+}
